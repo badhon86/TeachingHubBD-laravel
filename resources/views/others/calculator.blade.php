c920d2d31f20f08d8d801a7ea50fb9dbb2e38347
badhon sarkar <badhon35-2092@diu.edu.bd>
2021-02-03 23:06:53 +0600

    new page

---
+++ resources/views/others/calculator.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.index')
+@section('main')
+
+<div class="container">
+    <div class="section-title" data-aos="fade-up">
+        <h2>CALCULATOR</h2>
+        <p></p>
+    </div>
+</div>
+
+ <p>more specific for privacy-policy<a href="https://drive.google.com/file/d/1yYnnGZhgEkciWknvTmnbVE-WRjJIy2to/view?usp=sharing">ClickHere </a></p>
+
+    <pre>
+    Personal Data collected for the following purposes and using the following services:
+
+    Analytics
+    •	Google Analytics
+    Personal Data: Cookie and Usage Data
+    Google Analytics is a web analytics service that Google offers.
+    Google uses the data collected to track and examine the use of
+    www.example.com, prepare reports and share them with other Google
+    services on its activities. Google may use the data collected to
+    contextualize and personalize the ads of its own advertising network.
+    Personal data collected: Cookie and Usage Data.
+    Place of processing: the USA. Find Google’s privacy policy here.
+    •	You can opt out by clicking Opt Out
+
+    AdSense
+    •	Google AdSense is Google's advertisement service;
+    TeachingHub-BD.com Using the "DoubleClick" cookie that monitors the use of
+    TeachingHub-BD.com and the actions of users with regard to advertisements,
+     goods and services that we provide.
+    Collected personal data: Data about cookies and use.
+    Place of processing: the USA. You can find the AdSense Privacy Policy here.
+    •	You may decide to disable all the DoubleClick Cookies by clicking on:
+    google.com/settings/ads/onweb/optout?hl=en.
+
+    What do we do with your data?
+    User-related data is collected to allow the Application to provide its
+    servicesfor the following purposes: analytics and interaction with
+    external social networksand platforms; we do not use your data for any
+    other reason, by any means.
+
+    We do not sell or change your data, or use it in any other way.
+
+    Privacy
+    TeachingHub-BD.com Respects confidentiality. Any personal information that
+    you provideto us, including your name , address , telephone number and e-mail
+    address,will not be disclosed, sold or rented to any entity or individual other than
+    TeachingHub-BD.com
+
+    Credit card details
+    TeachingHub-BD.com Never ask for Credit Card information and ask you not to
+    enter them onany of the forms on TeachingHub-BD.com
+
+    External Sites.
+    TeachingHub-BD.com No responsibility for the content of external websites.
+    Before divulgingany personal information, you are recommended to read the
+    privacy policy of external sites.
+
+    Cookies
+    A "cookie" is a small text file of your browser data that allows
+    TeachingHub-BD.com to remember
+    you each time you visit this website (customization, etc.). Cookies themselves
+    do not contain anypersonal information, and TeachingHub-BD.com does not use cookies
+    to collect personal information.Cookies may also be used by 3rd party content
+    providers such as newsfeed.
+
+    Remember the Risks Whenever You Use the Internet
+    Although we do our best to protect your personal information, we cannot guarantee
+    that any informationyou send to TeachingHub-BD.com is safe, and you are solely responsible
+    for the confidentiality of anypasswords or other account details. In addition, other
+    Internet sites or services that may be accessible through TeachingHub-BD.com have separate
+    data and privacy practices independent of us, and therefore we disclaim any responsibility
+    or liability for their policies or actions.
+
+</pre>
+@endsection
