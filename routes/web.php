94602ada50cf8418568ba19213ae138c649ba8e1
badhon sarkar <badhon35-2092@diu.edu.bd>
2021-02-06 22:37:18 +0600

    nothing

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@
 
 
 Route::get('/', 'HomeController@index')->name('home'); // Home page.
-Route::post('/message', 'MessageController@store')->name('message'); // post message from page
-Route::get('privacy-policy', 'OtherPageController@getPrivacyPolicy')->name('privacy-policy'); // privacy and policy page
+Route::post('/message', 'MessageController@store')->name('message'); // post message from page.
+Route::get('privacy-policy', 'OtherPageController@getPrivacyPolicy')->name('privacy-policy'); // privacy and policy page.
 Route::get('terms-and-condition', 'OtherPageController@getTermsAndCondition')->name('terms-and-condition'); // terms and cndition page
 Route::get('calculate', 'OtherPageController@getCalculate')->name('calculate'); // terms and cndition page
 Route::get('contact', 'OtherPageController@getContact')->name('contact'); // contact page
