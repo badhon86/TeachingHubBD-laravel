52594a143753ed21f0a91560944dbcd703b57edd
badhon86 <badhon35-2092@diu.edu.bd>
2021-04-15 21:16:28 +0600

    discusss table.blade

---
+++ database/migrations/2021_04_15_150849_discuss.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Discuss extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discuss', function (Blueprint $table) {
+            
+            $table->timestamps();
+			$table->bigIncrements('id');
+			$table->string('Topic_name')->nullable();
+			$table->string('topic_image')->nullable();
+			$table->string('description')->nullable();
+			$table->string('upload')->nullable();
+			$table->string('your_name')->nullable();
+			
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discuss');
+    }
+}
