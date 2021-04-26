85d3e8c66e242dda74788c3704e18f74474bb11d
badhon86 <badhon35-2092@diu.edu.bd>
2021-04-26 22:47:24 +0600

    categories

--- app/Http/Controllers/AdminDiscussController.php
+++ app/Http/Controllers/AdminDiscussController.php
@@ -13,11 +13,11 @@ class AdminDiscussController extends CBController {
 
         $this->addDatetime("Created At","created_at")->required(false)->showIndex(false)->showAdd(false)->showEdit(false);
 		$this->addDatetime("Updated At","updated_at")->required(false)->showAdd(false)->showEdit(false);
-		$this->addText("Topic Name","Topic_name")->showEdit(false)->strLimit(150)->maxLength(255);
-		$this->addImage("Topic Image","topic_image")->showEdit(false)->encrypt(true);
-		$this->addTextArea("Description","description")->showEdit(false)->strLimit(150);
-		$this->addWysiwyg("Upload","upload")->required(false)->showEdit(false)->strLimit(150);
-		$this->addText("Your Name","your_name")->showEdit(false)->strLimit(150)->maxLength(255);
+		$this->addText("Topic Name","Topic_name")->strLimit(150)->maxLength(255);
+		$this->addImage("Topic Image","topic_image")->encrypt(true);
+		$this->addTextArea("Description","description")->strLimit(150);
+		$this->addWysiwyg("Upload","upload")->required(false)->strLimit(150);
+		$this->addText("Your Name","your_name")->strLimit(150)->maxLength(255);
 		$this->addWysiwyg("Reply","reply")->required(false)->showAdd(false)->strLimit(150);
 		
 
