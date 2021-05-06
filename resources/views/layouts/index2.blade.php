25af95c098752acfda35fbbced56d1cad815222f
badhon86 <badhon35-2092@diu.edu.bd>
2021-05-07 00:27:00 +0600

    categories

--- resources/views/layouts/index2.blade.php
+++ resources/views/layouts/index2.blade.php
@@ -10,7 +10,7 @@
     <link href="//fonts.googleapis.com/css2?family=Hind:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link href="//fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&display=swap" rel="stylesheet">
 
-
+a
     <link rel="stylesheet" href="assets/csses/style-starter.css">
   </head>
   <body>
