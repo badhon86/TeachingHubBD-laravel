f8ea2a3506c5d426c01babdbf4f84b986177b141
badhon86 <badhon35-2092@diu.edu.bd>
2021-05-11 00:15:46 +0600

    categories

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,3 @@ Route::get('discuss', 'OtherPageController@data')->name('discuss'); // blog page
 Route::get('discuspage', 'OtherPageController@getDisDetails')->name('discuspage');
 Route::get('contact', 'OtherPageController@getContact')->name('contact'); // contact page.
 Route::get('/{slug}', 'PageController@index')->name('page'); // all books pages
-
-a
