dc3fb3caa6725daae9dc15813fbed5b8e4b21d45
badhon86 <badhon35-2092@diu.edu.bd>
2021-05-21 01:20:23 +0600

    bot

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -61,7 +61,7 @@
 
     {{-- <script>(function(w, d) { w.CollectId = "60a694e8154f895c94bb96a2"; var h = d.head || d.getElementsByTagName("head")[0]; var s = d.createElement("script"); s.setAttribute("type", "text/javascript"); s.async=true; s.setAttribute("src", "https://collectcdn.com/launcher.js"); h.appendChild(s); })(window, document);</script> --}}
 
-a
+
 </head>
 
 <body>
