f1fd4524c3872eebda3d99af08e34e57899c6a5d
badhon86 <badhon35-2092@diu.edu.bd>
2021-05-21 00:55:27 +0600

    bot

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -37,11 +37,29 @@
     </style>
     @stack('css')
 
+<!-- Start of Async Callbell Code -->
+{{-- <script>
+    window.callbellSettings = {
+      token: "N3L9TQrxgiWitG46PRQZ86Y5"
+    };
+  </script>
+  <script>
+    (function(){var w=window;var ic=w.callbell;if(typeof ic==="function"){ic('reattach_activator');ic('update',callbellSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Callbell=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://dash.callbell.eu/include/'+window.callbellSettings.token+'.js';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
+  </script> --}}
+  <!-- End of Async Callbell Code -->
 
 
 
 
-    <script id="gs-sdk" src='//www.buildquickbots.com/botwidget/v3/demo/static/js/sdk.js?v=3' key="a7053131-0807-4b21-8383-80f1300c8b35" ></script>
+    {{-- <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+    <div class="elfsight-app-c5a6c38e-e791-416a-a837-0508f168e2c2"></div> --}}
+
+
+
+    {{-- <script id="gs-sdk" src='//www.buildquickbots.com/botwidget/v3/demo/static/js/sdk.js?v=3' key="a7053131-0807-4b21-8383-80f1300c8b35" ></script> --}}
+
+
+    <script>(function(w, d) { w.CollectId = "60a694e8154f895c94bb96a2"; var h = d.head || d.getElementsByTagName("head")[0]; var s = d.createElement("script"); s.setAttribute("type", "text/javascript"); s.async=true; s.setAttribute("src", "https://collectcdn.com/launcher.js"); h.appendChild(s); })(window, document);</script>
 
 
 </head>
@@ -74,6 +92,23 @@
     @stack('js')
 <!-- Go to www.addthis.com/dashboard to customize your tools -->
 <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-607b00e0ec0a0de4"></script>
+
+
+<<!-- GetButton.io widget -->
+{{-- <script type="text/javascript">
+    (function () {
+        var options = {
+            whatsapp: "+8801761374611", // WhatsApp number
+            call_to_action: "Message us", // Call to action
+            position: "right", // Position may be 'right' or 'left'
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+</script> --}}
+<!-- /GetButton.io widget -->
 
 
 
