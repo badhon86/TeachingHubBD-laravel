81957e197a1a303ad318030bab88dc11ae0f7f6f
badhon86 <badhon35-2092@diu.edu.bd>
2021-05-26 00:26:39 +0600

    admin-dash

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLDashboard</title>
+  <title>AdmDashboard</title>
 
 
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
