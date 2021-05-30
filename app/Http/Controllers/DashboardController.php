fc8a85f4d0687ebd348ea72f09d05b28504030a5
badhon86 <badhon35-2092@diu.edu.bd>
2021-05-31 01:25:41 +0600

    admin-dash

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,9 +6,13 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function getIndex() {
-        $data = [];
-        $data['page_title'] = "Dashboard";
-        return view("dashboard", $data);
+    // public function getIndex() {
+    //     $data = [];
+    //     $data['page_title'] = "Dashboard";
+    //     return view("dashboard", $data);
+    // }
+    public function getIndex()
+    {
+        return view('dashboard');
     }
 }
